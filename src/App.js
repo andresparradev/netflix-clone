7da1b6c993cdab884916fcc43ef3637465db0405
AndresParraGO <soyandresgarzon@gmail.com>
2021-10-15 19:02:37 -0500

    Install standardjs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-function App() {
+function App () {
   return (
     <div>
-      <h1>Netflix Clone   </h1>
+      <h1>Netflix Clone</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
